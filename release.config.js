1967ce130095f0c8f44e33add7ae392e65d30789
Imran Iqbal <iqbalmy@hotmail.com>
2019-02-17 20:23:35 +0000

    refactor: fix override `writerOpts.transform`

--- release.config.js
+++ release.config.js
@@ -1,83 +1,74 @@
-function getWriterOpts () {
-  return {
-    transform: (commit, context) => {
-      const issues = []
+transform: (commit, context) => {
+    const issues = []
 
-      commit.notes.forEach(note => {
+    commit.notes.forEach(note => {
         note.title = `BREAKING CHANGES`
-      })
+    })
 
-      if (commit.type === `feat`) {
+    if (commit.type === `feat`) {
         commit.type = `Features`
-      } else if (commit.type === `fix`) {
+    } else if (commit.type === `fix`) {
         commit.type = `Bug Fixes`
-      } else if (commit.type === `perf`) {
+    } else if (commit.type === `perf`) {
         commit.type = `Performance Improvements`
-      } else if (commit.type === `revert`) {
+    } else if (commit.type === `revert`) {
         commit.type = `Reverts`
-      } else if (commit.type === `docs`) {
+    } else if (commit.type === `docs`) {
         commit.type = `Documentation`
-      } else if (commit.type === `style`) {
+    } else if (commit.type === `style`) {
         commit.type = `Styles`
-      } else if (commit.type === `refactor`) {
+    } else if (commit.type === `refactor`) {
         commit.type = `Code Refactoring`
-      } else if (commit.type === `test`) {
+    } else if (commit.type === `test`) {
         commit.type = `Tests`
-      } else if (commit.type === `build`) {
+    } else if (commit.type === `build`) {
         commit.type = `Build System`
-      } else if (commit.type === `ci`) {
+    } else if (commit.type === `ci`) {
         commit.type = `Continuous Integration`
-      } else {
+    } else {
         return
-      }
+    }
 
-      if (commit.scope === `*`) {
+    if (commit.scope === `*`) {
         commit.scope = ``
-      }
+    }
 
-      if (typeof commit.hash === `string`) {
+    if (typeof commit.hash === `string`) {
         commit.hash = commit.hash.substring(0, 7)
-      }
+    }
 
-      if (typeof commit.subject === `string`) {
+    if (typeof commit.subject === `string`) {
         let url = context.repository
-          ? `${context.host}/${context.owner}/${context.repository}`
-          : context.repoUrl
+            ? `${context.host}/${context.owner}/${context.repository}`
+            : context.repoUrl
         if (url) {
-          url = `${url}/issues/`
-          // Issue URLs.
-          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
-            issues.push(issue)
-            return `[#${issue}](${url}${issue})`
-          })
+            url = `${url}/issues/`
+            // Issue URLs.
+            commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+                issues.push(issue)
+                return `[#${issue}](${url}${issue})`
+            })
         }
         if (context.host) {
-          // User URLs.
-          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+            // User URLs.
+            commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
             if (username.includes('/')) {
-              return `@${username}`
+                return `@${username}`
             }
 
             return `[@${username}](${context.host}/${username})`
-          })
+            })
         }
-      }
+    }
 
-      // remove references that already appear in the subject
-      commit.references = commit.references.filter(reference => {
+    // remove references that already appear in the subject
+    commit.references = commit.references.filter(reference => {
         if (issues.indexOf(reference.issue) === -1) {
-          return true
+            return true
         }
 
         return false
-      })
+    })
 
-      return commit
-    },
-    groupBy: `type`,
-    commitGroupsSort: `title`,
-    commitsSort: [`scope`, `subject`],
-    noteGroupsSort: `title`,
-    notesSort: compareFunc
-  }
+    return commit
 }
