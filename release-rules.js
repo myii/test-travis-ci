e6f5485bf06b7edf628256fe58d094a04232f521
Imran Iqbal <iqbalmy@hotmail.com>
2019-02-19 23:21:14 +0000

    build: test type with changes to `release-rules.js` and `Vx.x.x`

--- release-rules.js
+++ release-rules.js
@@ -16,4 +16,16 @@ module.exports = [
   {type: 'refactor', release: 'patch'},
   {type: 'perf', release: 'patch'},
   {type: 'test', release: 'patch'},
+
+  {type: 'build', release: 'patch'},
+  {type: 'ci', release: 'patch'},
+  {type: 'chore', release: 'patch'},
+  // {type: 'docs', release: 'patch'},
+  // {type: 'feat', release: 'patch'},
+  // {type: 'fix', release: 'patch'},
+  // {type: 'perf', release: 'patch'},
+  // {type: 'refactor', release: 'patch'},
+  {type: 'revert', release: 'patch'},
+  // {type: 'style', release: 'patch'},
+  // {type: 'test', release: 'patch'},
 ];
